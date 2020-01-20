2b8271177ec3636a68cefdbcb4e981644a557e33
Andre Costa <mr.luizandre@gmail.com>
2020-01-19 23:19:00 -0300

    Backend semana omnistack 10

---
+++ backend/src/index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const routes = require('./routes');
+
+const app = express();
+
+mongoose.connect('mongodb+srv://desenvolvimento:desenvolvimento@cluster0-kl48h.mongodb.net/week1-?retryWrites=true&w=majority',{
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+app.use(express.json());
+app.use(routes);
+
+app.listen(3333);
+
+// Métodos http: get, post, put, delete
+
+// Query params: req.query (filtros, ordenação, paginação)
+// Route params: req.params (identificar recurso alteração our emoção)
+// Body: req.body (dados para criação ou alteração de um registro)
+
+// MongoDB (não relacioanal)
