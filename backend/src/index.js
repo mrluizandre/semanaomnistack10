120fab7b176aac2a13f64fbdb721431bc6611de9
Andre Costa <mr.luizandre@gmail.com>
2020-01-20 12:13:10 -0300

    Permite acesso cors backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const routes = require('./routes');
 
 const app = express();
@@ -9,6 +10,7 @@ mongoose.connect('mongodb+srv://desenvolvimento:desenvolvimento@cluster0-kl48h.m
   useUnifiedTopology: true,
 });
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
